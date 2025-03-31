4d8b39446c949e2f9029c0dd63d4d4e5b8948545
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-03-31 14:43:08 -0300

    test: Adição testes unitários para download dos arquivos e criação do arquivo ZIP

---
+++ TestesNivelamento/src/test/java/com/intuitivecare/testes/ScraperTest.java
@@ -0,0 +1,76 @@
+package com.intuitivecare.testes;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class ScraperTest {
+
+    private static final String URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+    private static final String TEST_FOLDER = "TestesNivelamento/";
+    private static final String ZIP_FILE = TEST_FOLDER + "ArquivosCompactados.zip";
+
+    @Before
+    public void setUp() {
+        File dir = new File(TEST_FOLDER);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        // Limpar arquivos gerados após os testes
+        File[] arquivos = new File(TEST_FOLDER).listFiles();
+        if (arquivos != null) {
+            for (File arquivo : arquivos) {
+                if (!arquivo.getName().equals("ArquivosCompactados.zip")) { // Não apagar o arquivo zip para validar o teste
+                    arquivo.delete();
+                }
+            }
+        }
+
+        // Excluir arquivo ZIP após o teste
+        File zipFile = new File(ZIP_FILE);
+        if (zipFile.exists()) {
+            zipFile.delete();
+        }
+    }
+
+    @Test
+    public void testBaixarArquivosDesejados() throws IOException {
+        // Realizar o scraping e baixar os arquivos desejados
+        Scraper.baixarArquivos(Scraper.conectarUrl(URL));
+
+        // Verificar se os arquivos baixados existem e não estão vazios
+        String[] arquivosEsperados = {
+            "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf",
+            "Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf"
+        };
+
+        for (String nomeArquivo : arquivosEsperados) {
+            File arquivo = new File(TEST_FOLDER + nomeArquivo);
+            assertTrue("O arquivo não foi baixado: " + nomeArquivo, arquivo.exists());
+            assertTrue("O arquivo está vazio: " + nomeArquivo, arquivo.length() > 0);
+        }
+    }
+
+    @Test
+    public void testCompactarArquivos() throws IOException {
+        // Realizar o scraping e baixar os arquivos desejados
+        Scraper.baixarArquivos(Scraper.conectarUrl(URL));
+
+        // Compactar os arquivos baixados
+        Scraper.compactarArquivos(ZIP_FILE);
+
+        // Verificar se o arquivo ZIP foi gerado
+        File arquivoZip = new File(ZIP_FILE);
+        assertTrue("O arquivo ZIP não foi criado.", arquivoZip.exists());
+        assertTrue("O arquivo ZIP está vazio.", arquivoZip.length() > 0);
+    }
+}
